854570e9420de84c4af6325be805af165d724076
Jared <jaralleg@amazon.com>
2021-06-09 20:45:21 -0400

    setting up dice roller

--- fantasy-die-roller/src/components/DiceRoller.jsx
+++ fantasy-die-roller/src/components/DiceRoller.jsx
@@ -1,19 +1,33 @@
+//roll the dice
+//pass the result to the result list
+//allow the user to enter a name for the roll and save it
+
 import React from 'react';
 
-const DiceRoller = ({id, result, handleDieRoll}) => {
+const DiceRoller = ({id, handleDieRoll}) => {
 
     function handleDieDoll(min, max){
         min = Math.ceil(min);
         max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+        return Math.floor(Math.random() * (max - min + 1) + min); 
+        
       }
-      //console.log(handleDieDoll(1 , 8));
+      //const result = handleDieDoll(1 , 8);
     
     return (<div style={{border: "1px solid black"}}>
         <h1>Dice Roller</h1>
         <h3>{id}</h3>
-        <p>{result}</p>
-        <button onClick={handleDieRoll}>Roll!</button>
+        <label for="name">How many dice??:</label>
+        <input type="text" id="number" name="name" required
+        minlength="1" size="3"></input> 
+        <br></br>
+        <label for="name">How many faces?:</label>
+        <input type="text" id="type" name="name" required
+        minlength="1" size="3"></input>
+        <button onClick={handleDieRoll}>Roll your dice!</button>
+        <p>result{}</p>
+        
+      
     </div>
     )
 }
--- fantasy-die-roller/src/components/ResultList.jsx
+++ fantasy-die-roller/src/components/ResultList.jsx
@@ -1,9 +1,14 @@
+//Display the result
+
 import React from 'react';
 
 function ResultList(){
 return (
-<h1>Result List Component</h1>
-)
+    <>      
+    <h1>Result List</h1>
+    <p>result but in {}</p>
+    </>
+);
 }
 
 export default ResultList;
